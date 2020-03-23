378b85a7d6dbc809236e745b932b281bb3c27161
Tom Bishop <tomwbish@gmail.com>
2020-03-23 13:16:03 +1100

    chat leaderbaord

---
+++ FB/chat-leaderboard.R
@@ -0,0 +1,281 @@
+library(tidyverse)
+library(jsonlite)
+library(anytime)
+library(lubridate)
+library(highcharter)
+
+# Some Extra Stuff
+# ______________
+
+# Highcharts Theme
+source("https://raw.githubusercontent.com/TMBish/TMBisc/master/HC/hc_theme_tmbish.R")
+
+# Hoops Only Chat Data
+# ____________________
+
+#json = jsonlite::read_json("data/hoops-only-log-v1.json")
+
+json = jsonlite::read_json("data/corona-only-20200316.json")
+
+  list.files("data/hoops-only-20191112/", full.names = TRUE) %>%
+  map(~read_json(.))
+
+  
+  
+df = list.files("data/threads-20200323/", full.names=TRUE) %>%
+  map_dfr(function(t) {
+    read_json(t) %>%
+      pluck(., "messages") %>%
+      enframe() %>% 
+      mutate(
+        sender = value %>% map_chr("sender_name"),
+        content = value %>% map_chr(.f = function(x) {if(is.null(x$content)) return(NA); x$content}),
+        reactions = value %>% map_dbl(.f = function(x){if(is.null(x$reactions)) return(NA); length(x$reactions)}),
+        reactors = value %>% map(.f = function(x){if(is.null(x$reactions)) return(NULL); tibble(reactor = x$reactions %>% map_chr("actor"))}),
+        is_photo = value %>% map_lgl(.f = function(x){if(is.null(x$photo)) return(FALSE); TRUE}),
+        timestamp = value %>% map_chr("timestamp_ms"),
+        type = value %>% map_chr("type")
+      ) %>%
+      select(-name, -value) %>%
+      mutate(
+        datetime = 
+          timestamp %>% 
+          str_remove("\\.0+") %>%
+          str_remove("\\d{3}$") %>%
+          as.numeric() %>%
+          anytime(),
+        date = as_date(datetime)
+      ) %>%
+      mutate(
+        thread = 
+          t %>%
+          str_remove("data/threads-20200323/") %>%
+          str_remove("_\\d{1}.json$") %>%
+          str_replace_all("-", " ")
+      )
+  })
+
+# Graphs
+# ____________________
+
+df %>%
+  group_by(thread) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = thread, y = messages)) %>%
+  hc_title(text = "FB Message Threads") %>%
+  hc_yAxis(title = list(text = "Total Messages")) %>%
+  hc_xAxis(title = list(text = "")) %>%
+  hc_plotOptions(column = list(dataLabels = list(enabled = TRUE))) %>%
+  hc_add_theme(hc_theme_tmbish())
+
+
+df %>%
+  group_by(sender, thread) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = sender, y = messages, group = thread)) %>%
+  hc_title(text = "FB Message Threads") %>%
+  hc_yAxis(title = list(text = "Total Messages")) %>%
+  hc_xAxis(title = list(text = "")) %>%
+  hc_plotOptions(column = list(dataLabels = list(enabled = TRUE), stacking = "percent",  borderRadius = 0)) %>%
+  hc_add_theme(hc_theme_tmbish())
+
+df %>%
+  group_by(datetime) %>%
+  summarise(messages = n()) %>%
+  ungroup() %>%
+  mutate(
+    cumulative_posts = cumsum(messages),
+    datetime = highcharter::datetime_to_timestamp(datetime)
+  ) %>%
+  hchart("line", hcaes(x = datetime, y = cumulative_posts)) %>%
+  hc_title(text = "Corona Only") %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Total Posts")) %>%
+  hc_xAxis(
+    type = 'datetime',
+    labels = list(format = '{value: %d-%m}')
+  )
+
+
+df %>%
+  group_by(datetime, sender) %>%
+  summarise(messages = n()) %>%
+  ungroup() %>%
+  group_by(sender) %>%
+  mutate(
+    cumulative_posts = cumsum(messages),
+    datetime = highcharter::datetime_to_timestamp(datetime)
+  ) %>%
+  hchart("line", hcaes(x = datetime, y = cumulative_posts, group = sender)) %>%
+  hc_title(text = "Corona Only") %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Total Posts")) %>%
+  hc_xAxis(
+    type = 'datetime',
+    labels = list(format = '{value: %d-%m}')
+  )
+
+df %>%
+  filter(date >= "2019-09-01") %>%
+  group_by(sender) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = sender, y = messages)) %>%
+  hc_title(text = "Hoops Only") %>%
+  hc_subtitle(text = "2019-20 Season") %>%
+  hc_plotOptions(column = list(dataLabels = list(enabled = TRUE))) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Total Messages"))
+
+df %>%
+  filter(date >= "2019-09-01", type == "Share") %>%
+  group_by(date, sender) %>%
+  summarise(messages = n()) %>%
+  ungroup() %>%
+  group_by(sender) %>%
+  mutate(cumulative_posts = cumsum(messages)) %>%
+  hchart("line", hcaes(x = date, y = cumulative_posts, group = sender)) %>%
+  hc_title(text = "Hoops Only") %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Cumulative Shares"))
+
+df %>%
+  filter(date >= "2019-09-01") %>%
+  unnest(reactors) %>%
+  group_by(reactor) %>%
+  summarise(
+    reactions = n()
+  ) %>%
+  arrange(desc(reactions)) %>%
+  hchart("column", hcaes(x = reactor, y = reactions)) %>%
+  hc_title(text = "Hoops Only") %>%
+  hc_subtitle(text = "2019-20 Season") %>%
+  hc_plotOptions(column = list(dataLabels = list(enabled = TRUE))) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Total Reactions"))
+
+# React Rate
+df %>%
+  filter(date >= "2019-09-01") %>%
+  group_by(sender) %>%
+  summarise(
+    posts = n(),
+    reactions = sum(reactions, na.rm = TRUE)
+  ) %>%
+  mutate(
+    reaction_per_post = round((reactions / posts) * 100)
+  ) %>%
+  arrange(desc(reaction_per_post)) %>%
+  hchart("column", hcaes(x = sender, y = reaction_per_post)) %>%
+  hc_title(text = "Hoops Only") %>%
+  hc_subtitle(text = "2019-20 Season") %>%
+  hc_plotOptions(column = list(dataLabels = list(enabled = TRUE))) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Reactions Per 100 Posts"))
+
+
+
+df %>%
+  group_by(date) %>%
+  summarise(messages = n()) %>%
+  arrange(desc(messages)) %>%
+  head(20) %>%
+  mutate(date = as.character(date)) %>%
+  hchart("column", hcaes(x = date, y = messages), color = "#FF331F") %>%
+  hc_title(text = "20 highest message days")
+
+
+# Teams Based Logic
+# ____________________
+
+team_list = 
+  nbastatR::nba_teams() %>%
+  filter(isNonNBATeam == 0) %>%
+  select(nameTeam, teamName, cityTeam, teamNameFull) %>%
+  mutate_all(str_to_lower) %>%
+  filter(cityTeam != "la")
+
+
+# This takes ages \
+team_match = 
+  df %>%
+  regex_left_join(team_list, by = c("content" = "teamName")) %>%
+  regex_left_join(team_list, by = c("content" = "cityTeam"))
+
+team_match %>%
+  mutate(
+    team = nameTeam.x %>% coalesce(nameTeam.y)
+  ) %>%
+  select(content, team, sender) %>%
+  filter(!is.na(team)) %>%
+  filter(!(sender %in% c("James James"))) %>%
+  group_by(team, sender) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  group_by(team) %>%
+  mutate(total_messages = sum(messages)) %>%
+  ungroup() %>% 
+  arrange(desc(total_messages, messages)) %>%
+  hchart("column", hcaes(x = team, y = messages, group = sender)) %>%
+  hc_plotOptions(column = list(stacking = "normal")) %>%
+  hc_add_theme(hc_theme_flat()) %>%
+  hc_title(text = "Total Messages By NBA Team") %>%
+  hc_subtitle(text = 'Split by message sender')
+  
+  
+  
+team_match %>%
+  filter(sender == "Calham James") %>%
+  filter(!is.na(teamName)) %>%
+  group_by(teamName) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = teamName, y = messages), color = "#FF331F") %>%
+  hc_title(text = "Who Does Cal Talk About?")
+
+team_match %>%
+  filter(sender == "Max Massingham") %>%
+  filter(!is.na(teamName)) %>%
+  group_by(teamName) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = teamName, y = messages), color = "#FF331F") %>%
+  hc_title(text = "Who Does Max Talk About?")
+
+  
+team_match %>%
+  filter(sender == "Tom Bishop") %>%
+  filter(!is.na(teamName)) %>%
+  group_by(teamName) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = teamName, y = messages), color = "#FF331F") %>%
+  hc_title(text = "Who Does Tom Talk About?")
+
+team_match %>%
+  filter(sender == "Christian Burgin") %>%
+  filter(!is.na(teamName)) %>%
+  group_by(teamName) %>%
+  summarise(
+    messages = n()
+  ) %>%
+  arrange(desc(messages)) %>%
+  hchart("column", hcaes(x = teamName, y = messages), color = "#FF331F") %>%
+  hc_title(text = "Who Does Crit Talk About?")
+
+  
---
+++ NBA/triple-doubles.R
@@ -0,0 +1,258 @@
+library(tidyverse)
+library(nbastatR)
+library(highcharter)
+library(glue)
+library(future)
+
+# Getting Data
+# ______________
+
+plan(multiprocess) 
+
+gamelogs = nbastatR::game_logs(1985:2020)
+
+
+source("https://raw.githubusercontent.com/TMBish/TMBisc/master/HC/hc_theme_tmbish.R")
+
+# Charts
+# ______________
+
+df %>%
+  filter(pts >= 27) %>%
+  group_by(slugSeason) %>%
+  summarise(
+    scorers = n()
+  ) %>%
+  hchart("bar", hcaes(x = slugSeason, y = scorers)) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Players with 27+ ppg average")) %>%
+  hc_title(text = "27 Points Per Game")
+
+
+# Scoring Average Of Top 10 PPG Ranked Players
+df %>%
+  filter(numberRank <= 20) %>%
+  group_by(slugSeason) %>%
+  summarise(
+    ppg = mean(pts)
+  ) %>%
+  hchart("line", hcaes(x = slugSeason, y = ppg)) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_yAxis(title = list(text = "Players with 27+ ppg average")) %>%
+  hc_title(text = "27 Points Per Game")
+
+
+
+
+
+# APG
+# _______________
+
+df = metrics_leaders(
+  seasons = 2020,
+  metric = "ast",
+  modes = "PerGame"
+)
+
+df %>%
+  filter(numberRank <= 10) %>%
+  hchart("bar", hcaes(x = namePlayer, y = ast)) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_title(text = "19-20 Assists")
+
+
+
+# Isolating Key Players
+# ______________
+
+# Jon Isaac
+isaac = 
+  gamelogs %>%
+  filter(namePlayer == "Jonathan Isaac", typeSeason == "Regular Season") %>% 
+  arrange(desc(dateGame)) %>%
+  select(slugSeason, slugMatchup, namePlayer, stocks)
+
+# Summarise Postions: Centers vs Non Centers
+#   + Centers defined as one or more season with a C or F-C position label according to the rosters dataset
+playerPositions = 
+  rosters %>%
+  mutate(
+    isCenter = groupPosition %>% str_detect("C") %>% coalesce(FALSE)
+  ) %>%
+  group_by(namePlayer) %>%
+  summarise(
+    isCenter = max(isCenter)
+  )
+
+# Isolating Key Players
+#   + Players first X games (isaacs games so far)
+#   + Non centers
+#   + Steals and blocks per 36 minutes
+#   + Filter By Top 20 total stocks
+bigStockers = 
+  gamelogs %>%
+  inner_join(
+    playerPositions %>% filter(isCenter != 1),
+    by = "namePlayer"
+  ) %>%
+  group_by(namePlayer) %>%
+  arrange(dateGame) %>%
+  mutate(
+    game_number = row_number()
+  ) %>%
+  ungroup() %>%
+  filter(game_number <= nrow(isaac)) %>%
+  group_by(namePlayer) %>%
+  summarise(
+    first_season = min(yearSeason),
+    minutes = sum(minutes),
+    steals = sum(stl),
+    blocks = sum(blk),
+    stocks = sum(stocks),
+    games = n()
+  ) %>%
+  filter(first_season >= 2000) %>%
+  arrange(desc(stocks)) %>%
+  mutate(
+    rownumber = row_number()
+  ) %>%
+  filter(
+    rownumber <= 20 | namePlayer == "Matisse Thybulle"
+  )
+
+
+
+# Cumulative Stocks
+# __________________
+
+# Jon Isaac
+gamelogs %>%
+  inner_join(
+    bigStockers %>% select(namePlayer)
+  ) %>%
+  group_by(namePlayer) %>%
+  arrange(dateGame) %>%
+  mutate(
+    game_number = row_number()
+  ) %>%
+  ungroup() %>%
+  filter(game_number <= nrow(isaac)) %>%
+  group_by(namePlayer) %>%
+  arrange(dateGame) %>%
+  mutate(
+    cumulative_stocks = cumsum(stocks),
+    cumulative_minutes = cumsum(minutes)
+  ) %>%
+  select(namePlayer, game_number, stocks, cumulative_stocks, cumulative_minutes) %>%
+ # mutate(stocks_per_minute = 36 * (cumulative_stocks / cumulative_minutes)) %>%
+  #filter(game_number <= max(filter(., namePlayer == "Jonathan Isaac") %>% pull(game_number))) %>%
+  hchart("line", hcaes(x = game_number, y = cumulative_stocks, group = namePlayer)) %>%
+  hc_add_theme(high_tmbish())
+
+
+
+# Stocks Per36 Minutes
+# __________________
+
+
+stockBase = 
+  bigStockers %>%
+  mutate(
+    steals_per36 = (steals / minutes) * 36,
+    blocks_per36 = (blocks / minutes) * 36,
+    stocks_per36 = (stocks / minutes) * 36
+  ) %>%
+  arrange(desc(stocks_per36)) %>%
+  mutate(
+    color_group = case_when(
+      namePlayer == "Jonathan Isaac" ~ "Isaac",
+      namePlayer == "Matisse Thybulle" ~ "Thybulle",
+      TRUE ~ "Other"
+    )
+  ) %>%
+  mutate(
+    nameLabel = case_when(
+      namePlayer == "Matisse Thybulle" ~ str_c(namePlayer, "*"),
+      TRUE ~ namePlayer
+    )
+  ) %>%
+  mutate_at(
+    vars(matches("per36")),
+    ~round(.,1)
+  )
+
+thybulle = stockBase %>% filter(namePlayer == "Matisse Thybulle")
+
+# Player Pgotos
+player_photo = 
+  nbastatR::nba_players() %>%
+  select(namePlayer, urlPlayerThumbnail)
+
+# Creating The Face Series
+photoList = 
+  stockBase %>%
+  inner_join(player_photo) %>%
+  ungroup() %>%
+  mutate(
+    x = row_number()-1,
+    y = stocks_per36 + 0.5
+  ) %>%
+  select(x = x, y = y, urlPlayerThumbnail) %>%
+  pmap(
+    function(x, y, urlPlayerThumbnail) {
+      list(
+        x = x,
+        y = y,
+        marker = list(symbol = glue("url({urlPlayerThumbnail})"), height = 40, width = 55)
+      ) 
+    }
+  ) %>%
+  list(
+    name = "Player Photo",
+    data = .
+  ) %>%
+  list()
+
+# Ordered Stocks Column Chart
+stockBase %>%
+  hchart("column", hcaes(x = nameLabel, y = stocks_per36, color = colorize(color_group, c("#0077C0", "#D3D3D3", "#ED174C")))) %>%
+  hc_add_theme(hc_theme_tmbish()) %>%
+  hc_add_series_list(photoList) %>%
+  hc_chart(
+    spacingLeft = 20,
+    spacingRight = 20
+  ) %>%
+  hc_yAxis(
+    title = list(text = "Stocks Per 36 Minutes", style = list(fontSize = "1.8em")), 
+    lineWidth = 0, 
+    labels = list(enabled = FALSE),
+    max = max(stockBase$stocks_per36) + 0.5
+  ) %>%
+  hc_xAxis(
+    title = list(text = ""),
+    labels = list(style = list(fontSize = "1.2em"))
+  ) %>%
+  hc_title(text = "Big Stockers (Steals + Blocks)", align = "center") %>%
+  hc_subtitle(
+    text = "All non-centers, drafted since 2000, over their first 131 games (Isaac's games played at 2019-12-29)",
+    style = list(fontSize = "1.4em"),
+    align = "center"
+  ) %>%
+  hc_credits(
+    text = glue("*Smaller Sample: only {thybulle$games} games & {thybulle$minutes} minutes"), 
+    enabled = TRUE,
+    style = list(fontSize = "1.1em")
+  ) %>%
+  hc_plotOptions(
+    column = list(dataLabels = list(enabled = TRUE)),
+    line = list(lineWidth = 0)
+  )
+
+
+
+
+
+
+
+
+
