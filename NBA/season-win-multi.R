79378a7e18e02afaf9679ef8f914d9b21f39e453
Tom Bishop <tomwbish@gmail.com>
2023-02-16 17:55:09 +1100

    Season win multi reactable

--- NBA/season-win-multi.R
+++ NBA/season-win-multi.R
@@ -3,6 +3,7 @@ library(nbastatR)
 library(highcharter)
 library(glue)
 library(future)
+library(reactable)
 
 Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
 standings = nbastatR::standings(seasons = 2023)
@@ -17,7 +18,7 @@ bets = tribble(
   "Portland Trail Blazers", 39.5, "under"
 )
 
-# Record ------------------------------------------------------------------
+# Tracker ------------------------------------------------------------------
 
 win_rates = 
   standings %>% 
@@ -27,13 +28,6 @@ win_rates =
   mutate(gamesRemaining = (1-pctSeasonComplete) * 82) %>%
   inner_join(bets)
 
-win_rates
-  
-  
-# Cash out value ------------------------------------------------------------------
-
-pbinom(q = 26, size = 42, p = 0.667)
-
 
 win_rates %>%
   rowwise() %>%
@@ -41,7 +35,26 @@ win_rates %>%
     price = case_when(
       ou == "over" ~ 1/(1-pbinom(q = (line-wins), size = gamesRemaining, prob = pctWinTeam)),
       TRUE ~ 1/(pbinom(q = (line-wins), size = gamesRemaining, prob = pctWinTeam))
-    ) %>% round(2)
+    ) %>% round(2),
+    projectedWins = round(projectedWins, 1)
   ) %>%
-  select(nameTeam, projectedWins, line, ou, gamesRemaining, bishLivePrice = price)
+  select(nameTeam, gamesRemaining, ou, line, projectedWins, estimated_current_odds = price) %>%
+  reactable(
+    
+    compact=TRUE,
+    pagination = FALSE,
+    borderless = FALSE,
+    outlined = TRUE,
+    striped = FALSE,
+    defaultColDef = colDef(headerClass = "header",  align = "center", footerStyle = list(fontWeight = "bold")),
+    columns = list(
+      nameTeam = colDef(name = "Team"),
+      projectedWins = colDef(name = "Projected Wins", footer = "Multi Odds"),
+      line = colDef(name = "Line Bet"),
+      ou = colDef(name = "Side"),
+      gamesRemaining = colDef(name = "Games Remaining"),
+      estimated_current_odds = colDef(name = "Estimated Odds", footer = function(values) sprintf("$%.2f", prod(values)))
+    )
+  )
+
   
